7cce881160ba4848c05835cdcd745de44d3a31d8
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2024-11-13 17:25:02 +0700

    Initial Commit

---
+++ SEA/SEAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEAApp.swift
+//  SEA
+//
+//  Created by Kaung Khant Si Thu on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SEAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
